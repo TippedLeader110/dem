813ecbbb1e00c8289d56fa47a0d4b3e046e35d93
Ali Hidayat <40517187+usokun@users.noreply.github.com>
2020-02-29 16:23:57 +0700

    29 Feb
    Commit assets dll

---
+++ application/views/main/layout/carousel.php
@@ -0,0 +1,38 @@
+<div id="carouselFull" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+       <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+       <li data-target="#carouselIndicators" data-slide-to="1"></li>
+       <li data-target="#carouselIndicators" data-slide-to="2"></li>
+   </ol>
+   <div class="carousel-inner">
+       <div class="carousel-item active">
+           <img class="d-block" src="<?php echo base_url('assets/img/s1.jpg') ?>" alt="First slide">
+           <div class="carousel-caption d-md-block">
+               <h3>Welcome To Styles Bar and Grill</h3>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer felis neque, suscipit eget dolor quis, accumsan imperdiet elit. Praesent quis mauris</p>
+           </div>
+       </div>
+       <div class="carousel-item">
+           <img class="d-block" src="<?php echo base_url('assets/img/s2.jpg') ?>" alt="Second slide">
+           <div class="carousel-caption d-md-block">
+               <h3>Best Food and Wine Around</h3>
+               <p>Nullam at elementum felis, at sodales diam. In a lectus nisl. Maecenas sodales commodo sollicitudin. Sed nisi nisl, laoreet eu erat vel, porttitor scelerisque</p>
+           </div>
+       </div>
+       <div class="carousel-item">
+           <img class="d-block" src="<?php echo base_url('assets/img/s3.jpg') ?>" alt="Third slide">
+           <div class="carousel-caption d-md-block">
+               <h3>Award Winning and Friendly Service</h3>
+               <p>Nam scelerisque molestie cursus. Ut scelerisque turpis iaculis erat feugiat, eget tristique risus luctus. Quisque non est dignissim, rhoncus nisi a, fermentum</p>
+           </div>
+       </div>
+   </div>
+   <a class="carousel-control-prev" href="#carouselFull" role="button" data-slide="prev">
+       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+       <span class="sr-only">Previous</span>
+   </a>
+   <a class="carousel-control-next" href="#carouselFull" role="button" data-slide="next">
+       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+       <span class="sr-only">Next</span>
+   </a>
+</div>
