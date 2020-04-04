2ff6187dc5558095221ccbf8defdb3ee6197fcf9
nadiasitinamira <nadiasitinamira@gmail.com>
2020-04-04 12:35:44 +0700

    profil

--- application/views/repo/index/profile.php
+++ application/views/repo/index/profile.php
@@ -8,7 +8,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 </head>
-<body>
+<body data-spy="scroll" data-target="#navbarsExampleDefault">
 	<div class="cnavbar bscroll">
 		<div class="logo">
 			<a href="<?php echo base_url() ?>" style="color: white"><img class="logo" src="<?php echo base_url('assets/img/home/logo-pf.png') ?>" alt="DEM Indonesia" width="70" height="70"> DEM Indonesia</a>
@@ -23,47 +23,51 @@
               	</div>
         </div>
 	</div>
-	<div class="content" style="border-bottom: #f2f2f1 1px solid">
-		<div class="row">
-			<div class="col col-12 col-md-4">
-				<div class="card" align="center">
-					<div class="card-body">
-						<div class="page-header-image">
-							<img class="img" src="<?php echo base_url('assets/img/home/user.png') ?>">
-						</div>
-						<div class="field-wel">
-	          				<h4 style="color: white">
-	            			<b>user_id</b>
-	          				</h4>
+	<div class="row">
+		<div class="col col-12 col-md-12">
+	        <div class="card">
+	            <div class="card-body">
+	                <div class="page-header-image" align="center">
+	                    <img class="img" src="<?php echo base_url('assets/img/home/user.png') ?>">
+	                    <div class="field-wel">
+	          				<h4><b>Nadia Siti Namira</b></h4>
 	        			</div>
-        			</div>
-				</div>
-			</div>
-			<div class="col-12 col-md-8">
-				<div class=" tbl table-responsive">
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <div class="container">
+	    	<div class="row">
+		        <div>
 					<table class="table">
 						<tr>
-							<td>Nama</td>
+							<td><ul class="contentnode"><dl><dt>Jabatan</dt><dd>Karyawan</a></dd></dl></ul></td>
+							<td><a href="#">Edit</a></td>
 						</tr>
 						<tr>
-							<td>Jabatan</td>
+							<td><ul class="contentnode"><dl><dt>Email</dt><dd>nadiasitinamira@gmail.com</a></dd></dl></ul></td>
+							<td><a href="#">Edit</a></td>
+						</tr>
+						<tr><td></td><td></td></tr>
+					</table>
+				</div>
+				<div>
+					<table class="table">
+						<tr>
+							<td><ul class="contentnode"><dl><dt>Status</dt><dd>Aktif</a></dd></dl></ul></td>
+							<td><a href="#">Edit</a></td>
 						</tr>
 						<tr>
-							<td>Email</td>
+							<td><ul class="contentnode"><dl><dt>Telepon</dt><dd>0813-9719-8741</a></dd></dl></ul></td>
+							<td><a href="#">Edit</a></td>
 						</tr>
-						<tr>
-							<td>Telepon</td>
-						</tr>
-						<tr>
-							<td>Status</td>
-						</tr>
-						<tr>
-							<td></td>
-						</tr>
+						<tr><td></td><td></td></tr>
 					</table>
 				</div>
 			</div>
-		</div>
+	    </div>
+	</div>
+
 		<!--<div class="row">
 			<div class="col-12 col-md-12">
 				<div class="bg-white content-border shadow-sm">
@@ -78,9 +82,21 @@
 						</div>
 					</div>
 				</div>
+				<div class="bg-white content-border shadow-sm">
+					<h4>My Papers</h4>
+					<hr>
+					<div class="card">
+						<div class="card-header">
+							<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</h3>
+						</div>
+						<div class="card-body">
+							Date: View: Dll.
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>-->
-	</div>
+	
 
 	<script type="text/javascript">
 		$('#upload').click(function(event) {
